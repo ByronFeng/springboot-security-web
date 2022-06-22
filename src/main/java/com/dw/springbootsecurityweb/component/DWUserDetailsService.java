b9e3c22f4e22cd873c8a6adfb087efd5bc418a30
byron <1334630545@qq.com>
2022-06-22 23:45:15 +0800

    修改

--- src/main/java/com/dw/springbootsecurityweb/component/DWUserDetailsService.java
+++ src/main/java/com/dw/springbootsecurityweb/component/DWUserDetailsService.java
@@ -5,14 +5,15 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 /**
- *
- * 自定义
+ * 自定义实现 UserDetailsService
  *
  * Created by Byron on 2022/6/22.
  */
@@ -22,22 +23,33 @@ public class DWUserDetailsService implements UserDetailsService {
     private PasswordEncoder passwordEncoder;
 
     @Autowired
-    public void setPasswordEncoder(PasswordEncoder passwordEncoder){
+    public void setPasswordEncoder(PasswordEncoder passwordEncoder) {
         this.passwordEncoder = passwordEncoder;
     }
 
 
+    /**
+     * 从库中查询用户
+     */
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-        UserDetails rel = null;
-        UserDetails admin = User.withUsername("cat").password(new BCryptPasswordEncoder().encode("123")).roles("ADMIN").build();
-        UserDetails user = User.withUsername("tom").password(passwordEncoder.encode("123")).roles("USER").build();
-        if(username.equals("cat")){
-                rel = admin;
-        }else if(username.equals("tom")){
-            rel=user;
-        }
-        return rel;
+       return quickLoadUserDetails(username);
     }
+
+    /**
+     * 快速模拟从数据库中查询数据
+     *
+     * @return
+     */
+    private UserDetails quickLoadUserDetails(String username) {
+
+        List<UserDetails> list = new ArrayList<>();
+        list.add(User.withUsername("cat").password(passwordEncoder.encode("123")).roles("ADMIN").build());
+        list.add(User.withUsername("tom").password(passwordEncoder.encode("123")).roles("USER").build());
+        Optional<UserDetails> op = list.stream().filter(u -> u.getUsername().equals(username)).findFirst();
+        return op.get();
+    }
+
+
 }
